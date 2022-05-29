d591b7cd55bb03b28545e76cff9a4f3a3b04f0fb
Martin Zimmermann <martin.zimmermann@tum.de>
2022-05-29 23:29:58 +0200

    increasing runtime

--- sbl3_DDPG.py
+++ sbl3_DDPG.py
@@ -2,7 +2,6 @@ import gym
 
 from stable_baselines3 import DDPG
 from stable_baselines3.common.evaluation import evaluate_policy
-from rewardWrapper import RewardWrapper
 
 import time
 import os
@@ -28,7 +27,7 @@ env.reset()
 model = DDPG('MlpPolicy', env, verbose=1, tensorboard_log=logdir)
 # Train the agent
 TIMESTEPS = 10000
-for i in range(1,60):
+for i in range(1,100):
     model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False, tb_log_name=f"DDPG-{timestamp}")
     model.save(f"{models_dir}/{TIMESTEPS*i}")       # save model every 10000 steps
 
--- sbl3_load.py
+++ sbl3_load.py
@@ -6,27 +6,44 @@ from stable_baselines3.common.evaluation import evaluate_policy
 import datetime
 import os
 
+import imageio
+import numpy as np
+
 env = gym.make('LunarLanderContinuous-v2')
 env.reset()
 
 ### define model directory
-models_dir = "models/DDPG-1653841974"
+models_dir = "models/DDPG-1653853310"
 
 ### define step 
-model_path = f"{models_dir}/570000.zip"
+model_path = f"{models_dir}/980000.zip"
 
 # model = DQN.load(model_path, env=env)
 model = DDPG.load(model_path, env=env)
 
+rewards_ep = []
+
+images = []
+img = model.env.render(mode='rgb_array')
 
 episodes = 10 
 
 for ep in range(episodes):
     obs = env.reset()
     done = False
+    rewards = []
     while not done:
+        images.append(img)
         action, _states = model.predict(obs, deterministic=True)
-        obs, rewards, done, info = env.step(action)
+        obs, reward, done, info = env.step(action)
         env.render()
+        img = model.env.render(mode='rgb_array')
+        rewards.append(reward)
+    
+    print(f"episode: {ep} reward sum: {np.array(rewards).sum()}")
+    rewards_ep.append(np.array(rewards).sum())
 
+print(f"reward mean: {np.array(rewards_ep).mean()}")
+
+imageio.mimsave('lander_ddpg-1653853310.gif', [np.array(img) for i, img in enumerate(images) if i%2 == 0], fps=29)
 env.close()
